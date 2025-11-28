35b0adc7278dd08742a6482b940991b577e44811
CHINTHA VIJAYA DURGA <chinthavijayadurga@CHINTHAs-MacBook-Air.local>
2025-11-28 11:56:31 +0530

    size 1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -132,7 +132,7 @@ export default function Home() {
 
       {more.length > 0 && (
         <section className="mt-8">
-          <h2 className="text-2xl font-semibold mb-3 text-slate-800">
+          <h2 className="text-4xl font-semibold mb-3 text-slate-800">
             More stories
           </h2>
           <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
